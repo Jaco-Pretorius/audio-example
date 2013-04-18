f641ccc8ee419857622f36b10892cb35b340da9e
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-18 18:38:13 -0400

    Initial Commit

---
+++ audio-example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  audio-example
+//
+//  Created by Jaco on 4/18/13.
+//  Copyright (c) 2013 Jaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

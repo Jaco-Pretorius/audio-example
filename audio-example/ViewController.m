746b86726fab3196144a10104a1c7da08e30f908
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-18 19:18:53 -0400

    Adding record functionality

--- audio-example/ViewController.m
+++ audio-example/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *recordButton;
+@property BOOL isRecording;
+@property (strong, nonatomic) AVAudioRecorder *recorder;
 @end
 
 @implementation ViewController
@@ -17,13 +20,89 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.isRecording = NO;
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)toggleRecord
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (!self.isRecording)
+        [self startRecording];
+    else
+        [self stopRecording];
+}
+
+- (IBAction)playRecording
+{
+}
+
+- (void)startRecording
+{
+    if (![self startAudioSession])
+        return;
+    
+    NSError *err = nil;
+    self.recorder = [[ AVAudioRecorder alloc] initWithURL:self.createDatedRecordingFile settings:self.recordSettings error:&err];
+    if(!self.recorder){
+        NSLog(@"Could not create recorder: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        return;
+    }
+    
+    [self.recorder prepareToRecord];
+    [self.recorder record];
+    self.isRecording = YES;
+    [self.recordButton setTitle:@"Stop Recording" forState:UIControlStateNormal];
+}
+
+- (void)stopRecording
+{
+    [self.recorder stop];
+    self.isRecording = NO;
+    [self.recordButton setTitle:@"Record" forState:UIControlStateNormal];
+}
+
+- (BOOL)startAudioSession
+{
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *err = nil;
+    [audioSession setCategory :AVAudioSessionCategoryPlayAndRecord error:&err];
+    if(err){
+        NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        return NO;
+    }
+    
+    [audioSession setActive:YES error:&err];
+    err = nil;
+    if(err){
+        NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        return NO;
+    }
+    
+    BOOL audioHWAvailable = audioSession.inputAvailable;
+    if (! audioHWAvailable) {
+        NSLog(@"Audio input hardware not available");
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (NSDictionary *)recordSettings
+{
+    NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] init];
+    
+    [recordSettings setValue :[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSettings setValue:[NSNumber numberWithFloat:24000.0] forKey:AVSampleRateKey];
+    [recordSettings setValue:[NSNumber numberWithInt: 1] forKey:AVNumberOfChannelsKey];
+    
+    return recordSettings;
+}
+
+- (NSURL *)createDatedRecordingFile
+{
+    NSDate *now = [NSDate dateWithTimeIntervalSinceNow:0];
+    NSString *caldate = [now description];
+    NSString *recorderFilePath = [NSString stringWithFormat:@"%@/%@.m4a", [NSHomeDirectory() stringByAppendingPathComponent:@"tmp"], caldate];
+    return [NSURL fileURLWithPath:recorderFilePath];
 }
 
 @end
